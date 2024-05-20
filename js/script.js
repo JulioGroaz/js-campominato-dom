6a902e6f3a7eb896f65d830e572110cc5a45035b
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-20 14:20:09 +0200

    non è giusto la logica che sta dietro alle bombe

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ document.getElementById('grid-generator').addEventListener('click', function(){
         }
         return bombs;
     }
-    const bombs = generateBombs(celleTotali, bombeTotali); //controllo in console
+    const bombs = generaBombe(celleTotali, bombeTotali); //controllo in console
     console.log('Bombs:', bombs);
 
     for(let i=0 ; i<celleTotali; i++){
@@ -30,11 +30,39 @@ document.getElementById('grid-generator').addEventListener('click', function(){
         quadrati.append(i +1);
 
         quadrati.addEventListener('click', function(){  //qui creo una click function che mi permette di cambiare colore alle caselle una volta che vengono cliccate.
+            if(gameEnded){ //se il gioco è finito allora non fare nulla
+                return
+            } else if(bombs.includes(i)){ //se all'interno della cella cliccata è presente una bomb allora non hai vinto.
+                quadrati.classList.add('bomb');
+                endGame(false);
+            } else{ 
+                if(!quadrati.classList.contains('active')){// controllo che non contenga già la classe active quindi se non è gia stato cliccato
+                    quadrati.classList.add('active'); //allora metti in active e
+                    punteggio++; // e aumenta di uno il punteggio
+
+                    if(punteggio === celleTotali - bombeTotali){ // se tutte le celle sono state cliccate allora hai vinto.
+                        endGame(true);
+                    }
+
+                }
+            }
             quadrati.classList.add('active');
         });
 
         gridContainer.appendChild(quadrati);
     }
-
-
-})
+    //creo una funzion per la vittoria
+    function endGame(won) {
+        gameEnded = true;
+        let esito = '';
+    
+        if (won) {
+            esito = `HAI VINTO! IL TUO PUNTEGGIO è DI: ${punteggio}`;
+        } else {
+            esito = `HAI PERSO... IL TUO PUNTEGGIO è DI: ${punteggio}`;
+        }
+    
+        document.getElementById('esito').innerText = esito;
+    }
+    
+});
